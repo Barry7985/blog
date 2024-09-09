0c982324ff22fad6aa243517911beb388793a392
Mamadou Dioulde Barry <brry00802@gmail.com>
2024-09-09 23:13:36 +0100

    modification admisite

--- article/admin.py
+++ article/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from article.models import Article
+from article.models import Article,Comment
 # Register your models here.
 admin.site.register(Article)
+admin.site.register(Comment)
