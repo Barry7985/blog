45e43b9bc9d1f04cacdd67e8a3c7870cd5b212ab
Mamadou Dioulde Barry <brry00802@gmail.com>
2024-09-13 14:44:45 +0100

    configuration de mysql online pythonanywhere

--- blog/prod.py
+++ blog/prod.py
@@ -1,3 +1,14 @@
 from .settings import *
 
 ALLOWED_HOSTS = ['dioulde.pythonanywhere.com',]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dioulde$default',
+        'USER': 'dioulde',
+        'PASSWORD': 'kindy2403',
+        'HOST' : 'dioulde.mysql.pythonanywhere-services.com',
+        'PORT' : '3306',
+    }
+}
