9a0043455d910034dc3499a1106030f6558b52c9
Mamadou Dioulde Barry <brry00802@gmail.com>
2025-02-03 20:19:14 +0000

    feat: register Category model in admin and define CategoryAdmin class

--- article/admin.py
+++ article/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from article.models import Article,Comment
+from article.models import Article,Comment,Category
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Comment)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
